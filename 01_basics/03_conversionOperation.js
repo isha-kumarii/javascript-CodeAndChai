2780450377a06f70554032e925ec9dd6a3c8627c
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2023-12-28 15:39:51 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = null
+
+console.log(typeof(score))
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" -> 33
+//"33abc" -> NaN
+//"true" -> 1
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
