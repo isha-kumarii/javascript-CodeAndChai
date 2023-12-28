96d866ee9132d4717e10d4bb729ed32a0660ac14
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2023-12-28 15:28:53 +0000

    DataTypes

---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";   //treat all JS code as newer version
+
+//alert("Hello");   //we are using Nodejs , Not browser
+
+let name = "Isha"
+let age = 22
+let isLoggedIn = false
+let state = null
+//Number
+//bigint
+//string -> ""
+//boolean -> T/F
+//null -> standalone value
+//symbol -> unique
+
+
+//Object 
+
+console.log(typeof undefined);
+console.log(typeof null);
