f78b1e933903291b7d5d39f95e4008ea71940df3
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2024-01-07 07:50:36 +0000

    Arrays done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+//array
+
+const myArr = [0,1,2,3,4,5,6]
+const myArr2 = new Array(1,2,3,4,5,6)
+
+
+//Array methods
+myArr.push(9);
+myArr.pop()
+
+myArr.unshift(6)
+
+//Slice, Splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B ",myArr);
