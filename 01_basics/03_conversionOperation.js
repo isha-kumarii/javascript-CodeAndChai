3d967bc1e8ee91def4b58c0dd889fa3b14e0b454
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2024-01-07 07:11:13 +0000

    datatypes_Summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof(score))
+//console.log(typeof(score))
 
 let valueInNumber = Number (score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" -> 33
 //"33abc" -> NaN
@@ -15,4 +15,14 @@ console.log(valueInNumber)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let str1 ="hello"
+let str2 = " Isha"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1" +2 +2); //Output 122
+console.log(1+2+"2");   //Output 32
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Isha";
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const url = "https://fuzzy-invention-pjr7gg9wv7qq37g9.github.dev/"
+
+console.log(url.replace('//','=='))
