6c1f6cc779f849cc1b064eaa546fce87aacdcbc2
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2024-03-06 18:03:03 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+function sayMyName(){
+    console.log("I");
+}
+
+//sayMyName();
+
+function addTwoNUmbers(number1,number2){
+    let result = number1 + number2;
+    return result;
+}
+
+let r = addTwoNUmbers(3,"7");
+//console.log(addTwoNUmbers(4,5));
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`;
+}
+//console.log(loginUserMessage());    //Undefined just logged in.
+
+function calculateCartPrice(...num1){ //
+    return num1;
+}
+
+//console.log(calculateCartPrice(200,400, 1000)); 
+
+const user = {
+    username: "ISha",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.username}`);
+
+}
+handlesObject(user);
