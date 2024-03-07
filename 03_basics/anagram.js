7b856969f14406c11d1755882131c063c965a2e6
isha-kumarii <139695074+isha-kumarii@users.noreply.github.com>
2024-03-07 08:01:51 +0000

    IT Lab assignments

---
+++ 03_basics/anagram.js
@@ -0,0 +1,18 @@
+function checkStringsAnagram(a, b){
+    let len1 = a.length;
+    let len2 = b.length;
+    if(len1 != len2){
+        console.log("Invalid Input");
+        return;
+    }
+    let str1 = a.split('').sort().join('');
+    let str2 = a.split('').sort().join('');
+    if(str1 == str2){
+        console.log("True");
+    }
+    else{
+        console.log("False");
+    }
+    
+}
+checkStringsAnagram("Isha", "ashi");
---
+++ 03_basics/findAnagrams.js
@@ -0,0 +1,8 @@
+let i=0;
+let j = 0;
+function count(j){
+    for(i = 0; i<5; i++){
+         j = j+2;
+    }
+}
+console.log(count(4));
